a5484016a26b33ea6c61ffbb54658d69c9c30db6
imen Ghzaiel <imengzaiel@gmail.com>
2017-11-16 20:21:40 +0100

    Add Categorie Entity end expose /categories web service . Work done by BEN MAHMOUD Najia .

---
+++ src/AppBundle/Entity/Categorie.php
@@ -0,0 +1,135 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="categorie")
+ */
+class Categorie  {
+    //put your code here
+     /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+      /**
+     * @ORM\Column(type="string")
+     */
+    private $description;
+      /**
+     * @ORM\Column(type="string")
+     */
+    private $picture;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Product", mappedBy="categorie")
+     */
+    private $produits;
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->produits = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Categorie
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set picture
+     *
+     * @param string $picture
+     *
+     * @return Categorie
+     */
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * Get picture
+     *
+     * @return string
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    /**
+     * Add produit
+     *
+     * @param \AppBundle\Entity\Product $produit
+     *
+     * @return Categorie
+     */
+    public function addProduit(\AppBundle\Entity\Product $produit)
+    {
+        $this->produits[] = $produit;
+
+        return $this;
+    }
+
+    /**
+     * Remove produit
+     *
+     * @param \AppBundle\Entity\Product $produit
+     */
+    public function removeProduit(\AppBundle\Entity\Product $produit)
+    {
+        $this->produits->removeElement($produit);
+    }
+
+    /**
+     * Get produits
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getProduits()
+    {
+        return $this->produits;
+    }
+}
